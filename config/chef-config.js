247274f9251bbf131b66aa66b04ccbf95c9753db
Anshul Srivastava <anshul.srivastava@relevancelab.com>
2014-02-21 15:03:12 +0530

    added chef config file

---
+++ config/chef-config.js
@@ -0,0 +1,17 @@
+module.exports.knifeConfig = {
+	instancePemFile : "/home/anshul/devopstest-us-west-2.pem",
+	instanceUserName:"root",
+	knifeCWD : "/home/anshul/chef-repos/anshul/chef-repo"
+}
+
+module.exports.chefConfig = {
+	user_name: "anshul",
+	key_path: "./config/anshul.pem",
+	url: "https://api.opscode.com/organizations/anshul_rl"
+
+	/*
+	user_name: "adityakaranjkar",
+	key_path: "./config/adityakaranjkar.pem",
+	url: "https://api.opscode.com/organizations/adityacheforg"
+	*/
+}
