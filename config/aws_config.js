ec272710b069074f3963473b563b6ca6f4074531
Anshul Srivastava <anshul.srivastava@relevancelab.com>
2014-02-27 17:48:32 +0530

    hardcoded pmc sierra

--- config/aws_config.js
+++ config/aws_config.js
@@ -15,9 +15,9 @@ module.exports = {
 */
 
 var awsSettings = {
-	access_key : "AKIAI5JPZ6FOH4K3NQXQ",
-	secret_key : "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",
-	region : "us-east-1",
+	access_key : "AKIAJFEXRU3MVMRJEBFA",
+	secret_key : "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",
+	region : "us-west-2",
 	keyPairName:"devopstest",
 	securityGroupId : "sg-c00ee1a5",
 	pemFileLocation : "/home/anshul/",
