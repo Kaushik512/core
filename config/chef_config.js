da80f75ae1dbef8e5ea8021cfdd152b7336eaa61
Anshul Srivastava <anshul.srivastava@relevancelab.com>
2014-08-20 18:08:59 +0530

    storing imported nodes in db

--- config/chef_config.js
+++ config/chef_config.js
@@ -8,11 +8,20 @@ module.exports = {
 	chefValidationPemFile:"anshul_rl-validator.pem",
 	hostedChefUrl: "https://api.opscode.com/organizations/anshul_rl"
 	*/
-
+/*
 	chefReposLocation: "/home/anshul/chef-repos/",
 	userChefRepoName : "adityakaranjkar",
 	chefUserName: "adityakaranjkar",
 	chefUserPemFile: "adityakaranjkar.pem",
 	chefValidationPemFile:"adityacheforg-validator.pem",
 	hostedChefUrl: "https://api.opscode.com/organizations/adityacheforg"
+    */
+
+    chefReposLocation: "/home/anshul/chef-repos/",
+	userChefRepoName : "pcjoshi9",
+	chefUserName: "pcjoshi9",
+	chefUserPemFile: "pcjoshi9.pem",
+	chefValidationPemFile:"pjlab-validator.pem",
+	hostedChefUrl: "https://api.opscode.com/organizations/pjlab"
+    
 }
--- routes/routes_chef.js
+++ routes/routes_chef.js
@@ -1,5 +1,6 @@
 var settingsController = require('../controller/settings');
 var Chef = require('../classes/chef');
+var instancesDao = require('../classes/instances');
 
 module.exports.setRoutes = function(app, verificationFunc) {
 
@@ -15,6 +16,45 @@ module.exports.setRoutes = function(app, verificationFunc) {
 				}
 			});
 		});
+	});
+
+
+	app.post('/chef/sync/nodes', verificationFunc, function(req, res) {
+		var reqBody = req.body;
+		var projectId = reqBody.projectId;
+		var count = 0;
+		for (var i = 0; i < reqBody.selectedNodes.length; i++) {
+			var node = reqBody.selectedNodes[i];
+			var instance = {
+				projectId: projectId,
+				envId: node.env,
+				runlist: node.runlist,
+				platformId: node.nodeName,
+				instanceIP: node.ip,
+				instanceState: 'running',
+				bootStrapStatus: 'success',
+				bootStrapLog: {
+					err: false,
+					log: 'success',
+					timestamp: new Date().getTime()
+				},
+				blueprintData: {
+					blueprintName: "chef import",
+				}
+
+			}
+
+			instancesDao.createInstance(instance, function(err, data) {
+				count++;
+				if (err) {
+					return;
+				}
+				if (count === reqBody.selectedNodes.length - 1) {
+					res.send(200);
+				}
+			});
+		}
+
 	});
 
 
