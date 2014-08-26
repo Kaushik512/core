ca9ab5cc1dbb18bc42df5b59b3e8dd0e776edf7d
Anshul Srivastava <anshul.srivastava@relevancelab.com>
2014-08-26 15:07:32 +0530

    skeleton for d4dmaster

---
+++ classes/d4dmasters/organizations.js
@@ -0,0 +1,19 @@
+
+
+
+function Orgs () {
+    
+    this.getOrgByName = function(name,callback){
+
+    };
+
+
+
+}
+
+function Env() {
+	
+}
+
+module.exports.orgs = new Orgs();
+module.exports.env = new Orgs();
