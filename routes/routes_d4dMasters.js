d926e709b1a3e30b567c152fb5187bc2a7f176aa
getting2vinod <getting2vinod@users.noreply.github.com>
2014-09-04 17:31:18 +0530

    Fixed upload issue
    Fixed upload issue

--- routes/routes_d4dMasters.js
+++ routes/routes_d4dMasters.js
@@ -137,6 +137,23 @@ module.exports.setRoutes = function(app, sessionVerification) {
 		res.end(JSON.stringify(uuid1));
 		console.log("sent response" + JSON.stringify('{"uuid":"' + uuid1 + '"}'));
 	});
+	var fs = require('fs');
+	var path = require('path');
+	 
+	fs.mkdirParent = function(dirPath, mode, callback) {
+	  //Call the standard fs.mkdir
+	  fs.mkdir(dirPath, mode, function(error) {
+	    //When it fail in this way, do the custom steps
+	    if (error && error.errno === 34) {
+	      //Create all the parents recursively
+	      fs.mkdirParent(path.dirname(dirPath), mode, callback);
+	      //And then the directory
+	      fs.mkdirParent(dirPath, mode, callback);
+	    }
+	    //Manually run the callback since we used our own callback to do all these
+	    callback && callback(error);
+	  });
+	};
 	
 	function saveuploadedfile(suffix,req){
 		console.log(req.body);
