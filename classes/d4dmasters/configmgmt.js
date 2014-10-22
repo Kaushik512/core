bb24d35b85a5c13ec1495f875b58905bfbc1d15c
getting2vinod <getting2vinod@gmail.com>
2014-10-22 17:21:16 +0530

    Included cookbook addition in templates

--- classes/d4dmasters/configmgmt.js
+++ classes/d4dmasters/configmgmt.js
@@ -245,6 +245,74 @@ function Configmgmt() {
         console.log('Final :' + accessibleFiles);
     };
 
+    this.getChefServerDetailsByChefServer = function(paramconfigname, callback) {
+        d4dModel.findOne({
+            id: '10'
+        }, function(err, d4dMasterJson) {
+            if (err) {
+                console.log("Hit and error:" + err);
+            }
+            if (d4dMasterJson) {
+                var chefRepoPath = '';
+                var configmgmt = '';
+                settingsController.getChefSettings(function(settings) {
+                    chefRepoPath = settings.chefReposLocation;
+                    console.log("Repopath:" + chefRepoPath);
+                    console.log("paramorgname :" + paramconfigname);
+                    var hasOrg = false;
+                    d4dMasterJson.masterjson.rows.row.forEach(function(itm, i) {
+                        console.log("found by org" + itm.field.length);
+                        for (var j = 0; j < itm.field.length; j++) {
+                            if (itm.field[j]["name"] == 'configname') {
+                                if (itm.field[j]["values"].value == paramconfigname) {
+                                    console.log("found: " + i + " -- " + itm.field[j]["values"].value);
+                                    hasOrg = true;
+                                    //Re-construct the json with the item found
+
+                                    var orgname = '';
+                                    var loginname = '';
+                                    //looping to get the orgname , loginname
+                                    for (var k = 0; k < itm.field.length; k++) {
+                                        if (itm.field[k]["name"].indexOf("login") >= 0)
+                                            loginname = itm.field[k]["values"].value + "/";
+                                        if (itm.field[k]["name"].indexOf("orgname") >= 0)
+                                            orgname = itm.field[k]["values"].value + "/";
+                                    }
+
+                                    for (var k = 0; k < itm.field.length; k++) {
+                                        if (itm.field[k]["name"].indexOf("filename") > 0) {
+                                            if (configmgmt == '')
+                                                configmgmt += "\"" + itm.field[k]["name"].replace('_filename', '') + "\":\"" + chefRepoPath + orgname + loginname + '.chef/' + itm.field[k]["values"].value + "\"";
+                                            else
+                                                configmgmt += ",\"" + itm.field[k]["name"].replace('_filename', '') + "\":\"" + chefRepoPath + orgname + loginname + '.chef/' + itm.field[k]["values"].value + "\"";
+
+                                        } else {
+                                            if (configmgmt == '')
+                                                configmgmt += "\"" + itm.field[k]["name"] + "\":\"" + itm.field[k]["values"].value + "\"";
+                                            else
+                                                configmgmt += ",\"" + itm.field[k]["name"] + "\":\"" + itm.field[k]["values"].value + "\"";
+                                        }
+                                    }
+                                    configmgmt += ",\"chefRepoLocation\":\"" + chefRepoPath + orgname + loginname + "\"";
+
+                                    configmgmt = "{" + configmgmt + "}";
+                                    //console.log(">>>>>>" + JSON.stringify(configmgmt));
+                                    configmgmt = JSON.parse(configmgmt);
+                                    return;
+                                }
+                            }
+
+                            // console.log();
+                        }
+                    }); // rows loop
+                    callback(null, configmgmt);
+                }); //setting closure
+
+            } else {
+                callback(true, null);
+            }
+        });
+    }
 
     this.getChefServerDetailsByOrgname = function(paramorgname, callback) {
         d4dModel.findOne({
