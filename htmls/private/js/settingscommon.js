6509666b177b769721f551654a69d1e988a4e474
srikanthv02 <srikanthv02@gmail.com>
2014-12-19 18:34:40 +0530

    Text Change in User Setup

--- htmls/private/js/settingscommon.js
+++ htmls/private/js/settingscommon.js
@@ -1071,7 +1071,8 @@ function isFormValid() {
                     var str = currCtrl.val();
                     if (/^([\w-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([\w-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$/.test(str) == false) {
                         isValid = false;
-                        errormessageforInput(currCtrl.attr('id'), "not a valid email address");
+                        //updating error message
+                        errormessageforInput(currCtrl.attr('id'), "Email address is required");
                         currCtrl.focus();
                     }
                     break;
