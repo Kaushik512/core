5e4457246ce60f1dbbe506a336b43450c7975a93
anshul srivastava <anshul_srivastava1987@yahoo.co.in>
2015-01-21 18:42:40 +0530

    partial code instance schema validation

--- server/model/dao/schema-validator.js
+++ server/model/dao/schema-validator.js
@@ -1,8 +1,30 @@
 var validate = require('mongoose-validator');
 
+
+function getLengthValidator(min, max, message) {
+    return validate({
+        validator: 'isLength',
+        arguments: [min, max],
+        message: message
+    });
+}
+
+function getRegExValidator(regEx, message) {
+    return validate({
+        validator: 'matches'
+        arguments: regEx,
+        message: message
+    });
+}
+
 var validators = {
-   
+
+    orgIdValidator: [getLengthValidator(1, 100, 'Invalid Org Id')],
+    projIdValidator: [getLengthValidator(1, 100, 'Invalid Project Id ')],
+    envIdValidator: [getLengthValidator(1, 100, 'Invalid Environment Id ')],
+    recipeValidator: [getRegExValidator(/^recipe[|role[.*]$/i, "Invalid Runlist format")], // need to investigate further
+    catalystUsernameValidator: [getLengthValidator(1, 100, 'Invalid Catalyst Username')]
+
 };
 
-module.exports = validators; 
-
+module.exports = validators;
