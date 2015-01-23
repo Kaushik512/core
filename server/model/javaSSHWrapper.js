0b2798e15c9f05f249532826fc6adb738f9229f4
anshul srivastava <anshul_srivastava1987@yahoo.co.in>
2015-01-23 14:55:18 +0530

    fixed merge conflict

--- server/model/javaSSHWrapper.js
+++ server/model/javaSSHWrapper.js
@@ -1,23 +1,24 @@
 var java = require('java');
 var Tail = require('tail').Tail;
 var appConfig = require('../config/app_config');
-var nodeExtend = require('node.extend');
+var extend = require('extend');
 var uuid = require('node-uuid');
 var fs = require('fs');
 
 var currentDirectory = __dirname;
 
 var indexOfSlash = currentDirectory.lastIndexOf("/");
-if(indexOfSlash === -1) {
+if (indexOfSlash === -1) {
     indexOfSlash = currentDirectory.lastIndexOf("\\");
 }
-var D4DfolderPath = currentDirectory.substring(0,indexOfSlash+1);
+var D4DfolderPath = currentDirectory.substring(0, indexOfSlash + 1);
 
 
 
 console.log(D4DfolderPath);
-java.classpath.push(D4DfolderPath+'/java/lib/jsch-0.1.51.jar');
-java.classpath.push(D4DfolderPath+'/java/classes');
+java.classpath.push(D4DfolderPath + '/java/lib/jsch-0.1.51.jar');
+java.classpath.push(D4DfolderPath + '/java/classes');
+//java.classpath.push('/home/anshul/eclipse-workspace/catalyst-ssh/bin');
 
 
 var defaults = {
@@ -49,10 +50,10 @@ function LogFileTail(logFile, onChangeCallback) {
 
 
 function JavaSSH(javaSSHInstance, options) {
-    
+
     /**
-    * @param: runlist, chef runlist
-    */ 
+     * @param: runlist, chef runlist
+     */
     this.execChefClient = function(runlist, overrideRunlist, onComplete, onStdOut, onStdErr) {
         var stdOutLogFile = options.tempDir + uuid.v4();
         var stdErrLogFile = options.tempDir + uuid.v4();
@@ -102,9 +103,33 @@ function JavaSSH(javaSSHInstance, options) {
                         return;
                     }
                     if (typeof onComplete === 'function') {
-                        onComplete(err,retCode );
+                        onComplete(err, retCode);
                     }
                 });
+                /*var cmdList = java.newArray("java.lang.String", ['ls','ps -ef','pwd']);
+                java.callMethod(javaSSHInstance, 'executeListOfCmds', cmdList, stdOutLogFile, stdErrLogFile, function(err, retCode) {
+                    // deleting log files
+                    if (tailStdOut) {
+                        tailStdOut.stopTailing();
+                        fs.unlink(stdOutLogFile);
+                    }
+                    if (tailStdErr) {
+                        tailStdErr.stopTailing();
+                        fs.unlink(stdErrLogFile);
+                    }
+                    if (err) {
+                        console.log("error in runnnig method");
+                        console.log(err);
+                        if (typeof onComplete === 'function') {
+                            onComplete(err, null);
+                        }
+                        return;
+                    }
+                    if (typeof onComplete === 'function') {
+                        onComplete(err,retCode );
+                    }
+                });*/
+
             });
 
         });
@@ -171,14 +196,15 @@ function JavaSSH(javaSSHInstance, options) {
 
 
 module.exports.getNewInstance = function(options, callback) {
-    options = nodeExtend(defaults, options);
+    var def = extend({}, defaults);
+    options = extend(def, options);
     if (options.password) {
         options.pemFilePath = null;
     } else {
         options.password = null;
     }
     console.log('Initializing class');
-    java.newInstance('com.relevancelab.catalyst.security.ssh.SSH', options.host, options.port, options.username, options.password, options.pemFilePath, function(err, javaSSHInstance) {
+    java.newInstance('com.relevancelab.catalyst.security.ssh.SSHExec', options.host, options.port, options.username, options.password, options.pemFilePath, function(err, javaSSHInstance) {
 
         if (err) {
             console.log(err);
