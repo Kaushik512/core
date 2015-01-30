4408b01e4c032614120d207f72f8f28814e0149a
anshul srivastava <anshul_srivastava1987@yahoo.co.in>
2015-01-30 12:16:54 +0530

    fixed bug in error_response.js

--- server/routes/error_responses.js
+++ server/routes/error_responses.js
@@ -13,7 +13,7 @@ module.exports = {
         error: getErrMsgObj('Server Behaved Unexpectedly')
     },
     instance:{
-    	notFound : getErrMsgObj('Instance Does Not Exist');
+    	notFound : getErrMsgObj('Instance Does Not Exist')
     }
 
 };
