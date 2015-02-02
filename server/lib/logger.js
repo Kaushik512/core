f94415dd6539cdc0cbf15fa6f6e02dc0558013bf
Raj Narayanan <rajagopalan.narayanan@relevancelab.com>
2015-02-02 14:54:56 +0530

    Fixes for the too many eventemitters issue

--- server/lib/logger.js
+++ server/lib/logger.js
@@ -8,51 +8,65 @@ mkdirp.sync(log_folder);
 
 winston.emitErrs = true;
 
+////////
 /**
- * This is how application level loggers are created in a specific module
+ * This is the single logger used for logging application level logs
+ */
+var logger = new winston.Logger({
+    transports: [
+        new winston.transports.DailyRotateFile({
+            level: 'debug',
+            datePattern: '.yyyy-MM-dd',
+            filename: 'catalyst.log',
+            dirname:log_folder,
+            handleExceptions: true,
+            json: true,
+            maxsize: 5242880, //5MB
+            maxFiles: 5,
+            colorize: true,
+            timestamp:true,
+            name:'cat-file-log'
+        }),
+        new winston.transports.Console({
+            level: 'debug',
+            handleExceptions: true,
+            json: false,
+            colorize: true,
+            name:'cat-console'
+        })
+    ],
+    exitOnError: false
+});
+
+/**
+ * This is how application level loggers are created in a specific module.
+ * This method is more efficient than returning one logger for each module.
  *
  * var logger = require('./lib/logger')(module);
  *
  * @param module - The the calling module.
  */
-function create_logger(module){
-    if(! module.filename ){
-        throw new Error("Expecting a valid module object.Got this instead >> "+ module);
+function create_logger(calling_module){
+    if(! calling_module.filename ){
+        throw new Error("Expecting a valid module object.Got this instead >> "+ calling_module);
     }
-    var label = path.basename(module.filename);
 
-    /**
-     * This is the logger used for logging application level logs
-     */
-    var logger = new winston.Logger({
-        transports: [
-            new winston.transports.DailyRotateFile({
-                level: 'debug',
-                datePattern: '.yyyy-MM-dd',
-                filename: 'catalyst.log',
-                dirname:log_folder,
-                handleExceptions: true,
-                json: true,
-                maxsize: 5242880, //5MB
-                maxFiles: 5,
-                colorize: true,
-                timestamp:true,
-                name:'cat-file-log',
-                label:label
-            }),
-            new winston.transports.Console({
-                level: 'debug',
-                handleExceptions: true,
-                json: false,
-                colorize: true,
-                name:'cat-console',
-                label:label
-            })
-        ],
-        exitOnError: false
-    });
+    // We will modify the args we send to logger's log methods.
+    // We want the file name as part of the logs !!
+    function change_args(args){
+        var label = "[" + path.basename(calling_module.filename) + "] ";
+        args[0]=label+args[0];
+        return args
+    }
 
-    return logger;
+    return {
+        info: function(msg){logger.info.apply(logger, change_args(arguments));}, 
+        debug: function(msg){logger.debug.apply(logger, change_args(arguments));}, 
+        warn: function(msg, vars){logger.warn.apply(logger, change_args(arguments));}, 
+        error: function(msg, vars){logger.error.apply(logger, change_args(arguments));}, 
+        log: function(msg, vars){logger.log.apply(logger, change_args(arguments));}
+    }
+    
 };// end create_logger
 
 /**
@@ -68,22 +82,14 @@ function create_express_logger(){
                 filename: 'access.log',
                 dirname:log_folder,
                 handleExceptions: true,
-                json: false,
+                json: true,
                 maxsize: 5242880, //5MB
                 maxFiles: 5,
                 colorize: true,
                 timestamp:true,
                 name:'express-file-log',
                 label:"Express"
-            })/*,
-            new winston.transports.Console({
-                level: 'debug',
-                handleExceptions: true,
-                json: false,
-                colorize: true,
-                name:'express-console',
-                label:"Express"
-            })*/
+            })
         ],
         exitOnError: false
     });
