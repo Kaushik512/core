6c184846c6107d95b4b6fa6c3a22e9eddb02041b
getting2vinod <vinod.nair@relevancelab.com>
2015-02-17 00:24:13 +0530

    Included data migration script in seed

---
+++ seed/mongoupdate.js
@@ -0,0 +1,20 @@
+//User Migration script
+db = connect("192.168.1.17:27017/devops_new");
+db.blueprints.update(
+	{users:["sd1"]},
+	{$set: {'users':["superadmin"]}},
+	{multi:true}
+	
+);
+db.instances.update({},{$addToSet :{users:"superadmin"}},{multi:true});
+db.d4dmastersnew.update({rowid:"61"},{$set :{userrolename:"Admin"}});
+db.d4dmastersnew.update({rowid:"62"},{$set :{userrolename:"Designer"}});
+db.d4dmastersnew.update({rowid:"63"},{$set :{userrolename:"Consumer"}});
+db.d4dmastersnew.update(
+	{id:"7"},
+	{$set: {'userrolename':"Admin"}},
+	{multi:true}
+);
+db.d4dmastersnew.update({loginname:"sd1"},{$set :{loginname:"superadmin"}});
+
+
