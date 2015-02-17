95d7ad71c8dc69387b7b5f987daa1416dfbd8276
getting2vinod <vinod.nair@relevancelab.com>
2015-02-17 18:42:11 +0530

    Included superadmin role removal

--- seed/mongoupdate.js
+++ seed/mongoupdate.js
@@ -1,5 +1,8 @@
 //User Migration script
-db = connect("localhost:27017/devops_new");
+var server = "localhost";
+var port = "27017";
+var dbname = "devops_new";
+db = connect(server + ':' + port + '/' + dbname);
 db.blueprints.update(
 	{users:["sd1"]},
 	{$set: {'users':["superadmin"]}},
@@ -10,6 +13,7 @@ db.instances.update({},{$addToSet :{users:"superadmin"}},{multi:true});
 db.d4dmastersnew.update({rowid:"61"},{$set :{userrolename:"Admin"}});
 db.d4dmastersnew.update({rowid:"62"},{$set :{userrolename:"Designer"}});
 db.d4dmastersnew.update({rowid:"63"},{$set :{userrolename:"Consumer"}});
+db.d4dmastersnew.remove({userrolename:"SUPERADMIN",id:"6"},true);
 db.d4dmastersnew.update(
 	{id:"7"},
 	{$set: {'userrolename':"Admin"}},
