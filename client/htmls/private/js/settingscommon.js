7db11a9bfc035a6aaeff78ce11fe08373e85177f
getting2vinod <vinod.nair@relevancelab.com>
2015-03-10 15:46:24 +0530

    Included Team

--- client/htmls/private/js/settingscommon.js
+++ client/htmls/private/js/settingscommon.js
@@ -926,7 +926,8 @@ function readform(formID) {
                                 // curSelect.append('<option value="' + item.field[k].values.value + '">' + item.field[k].values.value + '</option>');
                                 // alert("Added:" + item.field[i].values.value);
                               //  alert(item.rowid);
-                                addToSelectList(item[curInput.attr('datapath')],item.rowid, curInput);
+                            //  alert(JSON.stringify(item[curInput.attr('id')]));
+                                addToSelectList(item[curInput.attr('id')],item.rowid, curInput);
                          //   }
                        
                     });
@@ -1936,6 +1937,7 @@ function addToSelectList(txtVal,rowidval, inp) {
     var imgCheck = "<i class=\'ace-icon fa fa-check bigger-110 green\' style=\'padding-left:10px;padding-right:10px;visibility:hidden\' ></i>";
     var imgDed = "<button class=\'pull-right bordered btn-danger\' style=\'margin-right:10px\' onClick=\'removeFromCodeList(this);\' ></button>";
     if (txtVal != '' && typeof inp != "undefined") {
+       // alert(txtVal);
         inp.append('<label class=\"toggle font-sm\" ><input onclick=\'if($(this).is(\":checked\")) {$(this).closest(\"label\").css(\"background-color\",\"#eeeeee\");$(this).css(\"border-color\",\"#3b9ff3\");}else{$(this).closest(\"label\").css(\"background-color\",\"#ffffff\");$(this).css(\"border-color\",\"red\");}\' type=\"checkbox\" name=\"checkbox-toggle\" rowid=\"' + rowidval + '\" value=\"' + txtVal + '\" style=\"width:100%\"><i data-swchoff-text=\"NO\" data-swchon-text=\"YES\"></i>' + txtVal + '</label>');
         //inp.append('<div class=\'codelistitem\' style=\'margin-top:2px;padding-top:2px;border:1px solid #eeeeee; background-color:#eeeeee !important;height:26px;width:100%;cursor:pointer\'><p class=\'bg-success\'>' + imgCheck + txtVal + '</p></div>');
         $('.widget-main').css('height', ($('.widget-main').height() + 40) + "px");
