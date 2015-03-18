db653df88d9c60b0946e4443aff37f2f4072e9d6
Gobinda <gobinda.das@relevancelab.com>
2015-03-18 18:10:24 +0530

    Fixes for Provider,Jira and Image

--- server/model/d4dmasters/d4dmastersmodelnew.js
+++ server/model/d4dmasters/d4dmastersmodelnew.js
@@ -275,7 +275,7 @@ var d4dMastersProviders = new mongoose.Schema(
         keypairname:{type:String,required:true,trim:true},
         securitygroupid:{type:String,required:true,trim:true},
         providertype: {type:String,required:true,trim:true},
-        accountpemfile:{type:String,required:false,trim:true},
+        accountpemfile:{type:String,trim:true},
         rowid:{type:String,required:true, trim:true}},{collection:'d4dmastersnew'}
 );
 var d4dModelMastersProviders = mongoose.model('d4dModelMastersProviders',d4dMastersProviders,'d4dmastersnew');
