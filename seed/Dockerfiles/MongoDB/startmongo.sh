b0de35e7f249e1a6c04d717b50659a8bc734d8d1
getting2vinod <vinod.nair@relevancelab.com>
2015-03-25 19:12:14 +0530

    Added docker file for catalystapp and mongo

---
+++ seed/Dockerfiles/MongoDB/startmongo.sh
@@ -0,0 +1,7 @@
+mongod --fork --logpath /data/mongod.log --dbpath /data/db --noprealloc --smallfiles
+
+mkdir -p /home/ubuntu/development/catalyst && cd /home/ubuntu/development/catalyst 
+mkdir -p /home/ubuntu/development/catalyst && cd /home/ubuntu/development/catalyst && \
+git clone https://catalyst-engg:catalyst_123@github.com/RLIndia/D4D.git && cd D4D && git pull origin feature_engineered && cd server && rm -rf node-#modules
+
+mongorestore --db devops_new /home/ubuntu/development/catalyst/D4D/seed/mongodump/devops_new
