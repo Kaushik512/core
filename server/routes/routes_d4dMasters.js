582e9c7844977f2519470d3de5d965f5e864aee9
Gobinda Das <gobinda.das@relevancelab.com>
2015-06-16 13:38:06 +0530

    Fixes for superadmin and configmgmt

--- server/routes/routes_d4dMasters.js
+++ server/routes/routes_d4dMasters.js
@@ -1533,10 +1533,45 @@ module.exports.setRoutes = function(app, sessionVerification) {
     });
     app.post('/d4dMasters/getListFiltered/:masterid', function(req, res) {
         logger.debug("Enter post() for  /d4dMasters/getListFiltered/%s", req.params.masterid);
+        if(req.params.masterid === "10" && typeof req.body.orgname != "undefined"){
+            logger.debug("Request body   : ",JSON.stringify(req.body));
+            var orgName = req.body.orgname;
+            d4dModelNew.d4dModelMastersOrg.find({
+                orgname : orgName,
+                id : "1",
+                active : true
+            },function(err,anOrg){
+                if(err){
+                    logger.debug("Error occored to get Org.");
+                    return;
+                }
+                logger.debug("Got Org: >>>>> ",JSON.stringify(anOrg));
+                var query = {};
+                query['id'] = req.params.masterid;
+                query['orgname_rowid'] = anOrg[0].rowid;
+                d4dModelNew.d4dModelMastersConfigManagement.find(query, function(err, d4dMasterJson) {
+                    if (err) {
+                        logger.error("Hit and error:", err);
+                    }
+                    logger.debug("getListFiltered %s", d4dMasterJson.length);
+                    if (d4dMasterJson.length > 0) {
+                        logger.debug("sent response %s", JSON.stringify(d4dMasterJson));
+                        res.send("Found");
+                        logger.debug("Exit post() for  /d4dMasters/getListFiltered/%s", req.params.masterid);
+                    } else {
+                        logger.debug("sent response %s", JSON.stringify(d4dMasterJson));
+                        res.send("Not Found");
+                    }
+
+                });
+            });
+        }else{
+
         configmgmtDao.getDBModelFromID(req.params.masterid, function(err, dbtype) {
             if (err) {
                 logger.error("Hit and error:", err);
             }
+
             if (dbtype) {
                 var query = {};
                 // query['rowid'] = {
@@ -1578,6 +1613,7 @@ module.exports.setRoutes = function(app, sessionVerification) {
                 res.send(500);
             }
         });
+}
     });
 
     app.get('/d4dMasters/:masterid/:filtercolumnname/:filtercolumnvalue', function(req, res) {
