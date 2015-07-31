0e2d6cb0b71111e9bc7d9cbd2d9b93deb520afbe
jagadeesh12 <jagadeesh.manjunath@relevancelab.com>
2015-07-31 18:30:34 +0530

    New Blueprint changes

--- Test/SmokeTest/catframework/project_rl_catalyst/Chef/knife.rb
+++ Test/SmokeTest/catframework/project_rl_catalyst/Chef/knife.rb
@@ -3,9 +3,9 @@
 current_dir = File.dirname(__FILE__)
 log_level                :info
 log_location             STDOUT
-node_name                "mycatqa"
-client_key               "#{current_dir}/mycatqa.pem"
-validation_client_name   "cattest-validator"
-validation_key           "#{current_dir}/cattest-validator.pem"
-chef_server_url          "https://api.opscode.com/organizations/cattest"
+node_name                "jagadeesh12"
+client_key               "#{current_dir}/jagadeesh12.pem"
+validation_client_name   "jm012-validator"
+validation_key           "#{current_dir}/jm012-validator.pem"
+chef_server_url          "https://api.opscode.com/organizations/jm012"
 cookbook_path            ["#{current_dir}/../cookbooks"]
