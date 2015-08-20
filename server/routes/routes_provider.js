b3efa942274fb4c4a794a2d903967e0db882f673
getting2vinod <vinod.nair@relevancelab.com>
2015-08-20 09:40:51 +0530

    Intermediate

--- server/routes/routes_provider.js
+++ server/routes/routes_provider.js
@@ -370,6 +370,80 @@ module.exports.setRoutes = function(app, sessionVerificationFunc) {
         
     });
 
+
+
+    // Delete a particular AWS Provider.
+    app.delete('/openstack/providers/:providerId', function(req, res) {
+        logger.debug("Enter delete() for /openstack/providers/%s", req.params.providerId);
+        var user = req.session.user;
+        var category = configmgmtDao.getCategoryFromID("9");
+        var permissionto = 'delete';
+        var providerId = req.params.providerId.trim();
+        if (typeof providerId === 'undefined' || providerId.length === 0) {
+            res.send(500, "Please Enter ProviderId.");
+            return;
+        }
+
+        usersDao.haspermission(user.cn, category, permissionto, null, req.session.user.permissionset, function(err, data) {
+            if (!err) {
+                logger.debug('Returned from haspermission : ' + data + ' : ' + (data == false));
+                if (data == false) {
+                    logger.debug('No permission to ' + permissionto + ' on ' + category);
+                    res.send(401, "You don't have permission to perform this operation.");
+                    return;
+                }
+            } else {
+                logger.error("Hit and error in haspermission:", err);
+                res.send(500);
+                return;
+            }
+
+            masterUtil.getLoggedInUser(user.cn, function(err, anUser) {
+                if (err) {
+                    res.send(500, "Failed to fetch User.");
+                }
+                logger.debug("LoggedIn User:>>>> ", JSON.stringify(anUser));
+                if (anUser) {
+                    //data == true (create permission)
+                    /*if(data && anUser.orgname_rowid[0] !== ""){
+                    logger.debug("Inside check not authorized.");
+                    res.send(401,"You don't have permission to perform this operation.");
+                    return;
+                }*/
+
+                    VMImage.getImageByProviderId(providerId, function(err, anImage) {
+                        if (err) {
+                            logger.error(err);
+                            res.send(500, errorResponses.db.error);
+                            return;
+                        }
+                        if (anImage) {
+                            res.send(403, "Provider already used by Some Images.To delete provider please delete respective Images first.");
+                            return;
+                        }
+
+                        openstackProvider.removeopenstackProviderById(providerId, function(err, deleteCount) {
+                            if (err) {
+                                logger.error(err);
+                                res.send(500, errorResponses.db.error);
+                                return;
+                            }
+                            if (deleteCount) {
+                                logger.debug("Enter delete() for /providers/%s", req.params.providerId);
+                                res.send({
+                                    deleteCount: deleteCount
+                                });
+                            } else {
+                                res.send(400);
+                            }
+                        });
+                    });
+                }
+            });
+        }); //
+    });
+
+
     // Create AWS Provider.
     app.post('/aws/providers', function(req, res) {
 
