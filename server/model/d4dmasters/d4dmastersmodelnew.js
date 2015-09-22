f3081f67f59d1a24765d87e2c71fac0a2b385d31
gobindadas <gobinda.das@relevancelab.com>
2015-09-22 16:00:44 +0530

    Moved app deploy to project page

--- server/model/d4dmasters/d4dmastersmodelnew.js
+++ server/model/d4dmasters/d4dmastersmodelnew.js
@@ -148,6 +148,7 @@ var d4dMastersProjects = new mongoose.Schema(
 	environmentname_rowid: {type:String, trim:true},
 	description: {type:String, trim:true, validate:descValidator},
 	active: {type:Boolean, trim:true,default:true},
+	applicationname: {type:[String], trim:true},
 	/*teamname: {type:String, trim:true},
 	teamname_rowid: {type:String, trim:true},*/
 	rowid:{type:String,required:true, trim:true}},{collection:'d4dmastersnew'}
--- server/routes/routes_appdeploy.js
+++ server/routes/routes_appdeploy.js
@@ -12,6 +12,7 @@ var logger = require('_pr/logger')(module);
 var AppDeploy = require('_pr/model/app-deploy/app-deploy');
 var errorResponses = require('./error_responses');
 var AppData = require('_pr/model/app-deploy/app-data');
+var masterUtil = require('_pr/lib/utils/masterUtil.js');
 
 
 module.exports.setRoutes = function(app, sessionVerificationFunc) {
@@ -115,7 +116,7 @@ module.exports.setRoutes = function(app, sessionVerificationFunc) {
     // Get AppDeploy w.r.t. appName and env
     app.get('/app/deploy/env/:envId/list', function(req, res) {
         logger.debug("/app/deploy/env/:envId/list called...");
-        AppData.getAppDataWithDeployList(req.params.envId, function(err, appDeploy) {
+        masterUtil.getAppDataWithDeployList(req.params.envId, function(err, appDeploy) {
             if (err) {
                 res.send(500, errorResponses.db.error);
                 return;
@@ -183,7 +184,7 @@ module.exports.setRoutes = function(app, sessionVerificationFunc) {
 
     // Get all AppData by name
     app.get('/app/deploy/data/:appName/list', function(req, res) {
-        AppData.getAppDataByName(req.params.appName, function(err, appDatas) {
+        masterUtil.getAppDataByName(req.params.appName, function(err, appDatas) {
             if (err) {
                 res.send(500, "Please add app name.");
                 return;
