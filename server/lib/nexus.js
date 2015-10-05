c1c055cc01d0ccfab4b65d275e8b0b3477972795
gobindadas <gobinda.das@relevancelab.com>
2015-10-05 18:30:50 +0530

    Added api for nexus

--- server/lib/nexus.js
+++ server/lib/nexus.js
@@ -10,6 +10,8 @@
 
 var Client = require('node-rest-client').Client;
 var logger = require('_pr/logger')(module);
+var d4dModelNew = require('../model/d4dmasters/d4dmastersmodelnew.js');
+var parser = require('xml2json');
 
 var Nexus = function() {
     this.authenticateNexus = function(requestBody, callback) {
@@ -31,7 +33,105 @@ var Nexus = function() {
         // Handling Exception for nexus req.
         reqSubmit.on('error', function(err) {
             console.log('Something went wrong on req!!', err.request.options);
-            callback(err);
+            callback([]);
+        });
+    }
+
+    this.getNexusRepositories = function(anId, callback) {
+        d4dModelNew.d4dModelMastersNexusServer.find({
+            rowid: anId,
+            id: "26"
+        }, function(err, nexus) {
+            if (err) {
+                logger.debug(500, "Failed to fetch Nexus Server from DB.", err);
+                callback(err, null);
+            }
+            if (nexus.length) {
+                var options_auth = {
+                    user: nexus[0].username,
+                    password: nexus[0].nexuspassword
+                };
+                client = new Client(options_auth);
+                //var nexusUrl = nexus[0].hostname + '/service/local/all_repositories';
+                var nexusUrl = nexus[0].hostname + '/service/local/repositories';
+                client.registerMethod("jsonMethod", nexusUrl, "GET");
+                var reqSubmit = client.methods.jsonMethod(function(data, response) {
+                    //logger.debug("response: ", response);
+                    var json = parser.toJson(data);
+                    logger.debug("data: ", JSON.stringify(json));
+                    callback(null, json);
+                });
+            } else {
+                callback(null, null);
+            }
+        });
+    }
+
+    this.getNexusArtifact = function(anId, repoName, callback) {
+        d4dModelNew.d4dModelMastersNexusServer.find({
+            rowid: anId,
+            id: "26"
+        }, function(err, nexus) {
+            if (err) {
+                logger.debug(500, "Failed to fetch Nexus Server from DB.", err);
+                callback(err, null);
+            }
+            if (nexus.length) {
+                var options_auth = {
+                    user: nexus[0].username,
+                    password: nexus[0].nexuspassword
+                };
+                client = new Client(options_auth);
+                var nexusUrl = nexus[0].hostname + '/service/local/data_index?q=org.javaee7.sample';
+                client.registerMethod("jsonMethod", nexusUrl, "GET");
+                client.methods.jsonMethod(function(data, response) {
+                    //logger.debug("response: ", response);
+                    var json = parser.toJson(data);
+                    logger.debug("data: ", typeof json);
+                    json = JSON.parse(json);
+                    if (json) {
+                        var artifactList = [];
+                        var artifacts = json['search-results'].data.artifact;
+                        for (var i = 0; i < artifacts.length; i++) {
+                            if (repoName === artifacts[i].repoId) {
+                                artifactList.push(artifacts[i]);
+                            }
+                        }
+                    }
+                    callback(null, artifactList);
+                });
+            } else {
+                callback(null, null);
+            }
+        });
+    }
+
+    this.getNexusArtifactVersions = function(anId, repoName, reqBody, callback) {
+        d4dModelNew.d4dModelMastersNexusServer.find({
+            rowid: anId,
+            id: "26"
+        }, function(err, nexus) {
+            if (err) {
+                logger.debug(500, "Failed to fetch Nexus Server from DB.", err);
+                callback(err, null);
+            }
+            if (nexus.length) {
+                var options_auth = {
+                    user: nexus[0].username,
+                    password: nexus[0].nexuspassword
+                };
+                client = new Client(options_auth);
+                var groupId = reqBody.groupId.replace(/\./g, '/');
+                var nexusUrl = nexus[0].hostname + '/service/local/repositories/' + repoName + '/content/' + groupId + '/' + reqBody.artifactId + '/maven-metadata.xml';
+                client.registerMethod("jsonMethod", nexusUrl, "GET");
+                var reqSubmit = client.methods.jsonMethod(function(data, response) {
+                    logger.debug("nexusUrl: ", nexusUrl);
+                    logger.debug("data: ", JSON.stringify(data));
+                    callback(null, data);
+                });
+            } else {
+                callback(null, null);
+            }
         });
     }
 }
--- server/routes/routes_nexus.js
+++ server/routes/routes_nexus.js
@@ -13,10 +13,23 @@ var logger = require('_pr/logger')(module);
 module.exports.setRoutes = function(app, verificationFunc) {
     app.all('/nexus/*', verificationFunc);
 
+    // Authenticate Nexus.
     app.post('/nexus/authenticate', function(req, res) {
     	logger.debug("Called nexus authenticate");
+    	if(!req.body.hostname){
+    		res.send(500,"HostName can't be empty.");
+    		return;
+    	}
+    	if(!req.body.username){
+    		res.send(500,"UserName can't be empty.");
+    		return;
+    	}
+    	if(!req.body.nexuspassword){
+    		res.send(500,"Password can't be empty.");
+    		return;
+    	}
     	if(req.body.hostname.indexOf("http://") === -1){
-    		res.send(500);
+    		res.send(500,"Invalid hostname.");
     		return;
     	}
         nexus.authenticateNexus(req.body, function(data) {
@@ -29,4 +42,65 @@ module.exports.setRoutes = function(app, verificationFunc) {
             }
         });
     });
+
+    app.get('/nexus/:anId/repositories',function(req,res){
+    	logger.debug("Called nexus repositories..");
+    	if(!req.params.anId){
+    		res.send(500,"Nexus Id can't be empty.");
+    	}
+    	nexus.getNexusRepositories(req.params.anId,function(err,repositories){
+    		if(err){
+    			logger.debug("Error while fetching nexus repositories.");
+    			res.send(500,"Error while fetching nexus repositories.");
+    			return;
+    		}
+    		if(!repositories){
+    			res.send(404,"There is no Nexus Server configured.");
+    			return;
+    		}
+    		logger.debug("Got nexus repositories: ",JSON.stringify(repositories));
+    		res.send(repositories);
+    	});
+    });
+
+    app.get('/nexus/:anId/repositories/:repoName/artifact',function(req,res){
+    	logger.debug("Called nexus repositories..");
+    	if(!req.params.anId){
+    		res.send(500,"Nexus Id can't be empty.");
+    	}
+    	nexus.getNexusArtifact(req.params.anId,req.params.repoName,function(err,artifact){
+    		if(err){
+    			logger.debug("Error while fetching nexus artifact.");
+    			res.send(500,"Error while fetching nexus artifact.");
+    			return;
+    		}
+    		if(!artifact){
+    			res.send(404,"There is no Nexus Server configured.");
+    			return;
+    		}
+    		logger.debug("Got nexus artifact: ",JSON.stringify(artifact));
+    		res.send(artifact);
+    	});
+    });
+
+    app.post('/nexus/:anId/repositories/:repoName/artifact/versions',function(req,res){
+    	logger.debug("Called nexus repositories..");
+    	if(!req.params.anId){
+    		res.send(500,"Nexus Id can't be empty.");
+    	}
+    	nexus.getNexusArtifactVersions(req.params.anId,req.params.repoName,req.body,function(err,versions){
+    		if(err){
+    			logger.debug("Error while fetching nexus artifact versions.");
+    			res.send(500,"Error while fetching nexus artifact versions.");
+    			return;
+    		}
+    		if(!versions){
+    			res.send(404,"There is no Nexus Server configured.");
+    			return;
+    		}
+    		logger.debug("Got nexus artifact versions: ",JSON.stringify(versions));
+    		res.send(versions);
+    	});
+    });
+
 }
