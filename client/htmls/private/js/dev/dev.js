0b761438bb5c4f625cd12f5ba3a4d478967cfcd1
anshul srivastava <anshul_srivastava1987@yahoo.co.in>
2016-01-11 19:04:32 +0530

    fixed azure arm breadcrumb

--- client/htmls/private/js/dev/dev.js
+++ client/htmls/private/js/dev/dev.js
@@ -3952,6 +3952,43 @@ window.removeArmDeployment = function() {
                 }
 
             });
+            
+            $('.AzureArmTab').click(function(e) {
+                var getbreadcrumbul = $('#ribbon').find('.breadcrumb').find('li:lt(5)');
+                var getbreadcrumbullength = getbreadcrumbul.length;
+                var DummyBreadCrumb;
+                if (getbreadcrumbullength > 0) {
+                    //alert(getbreadcrumbullength);
+                    for (var counter = 0; counter < getbreadcrumbullength; counter++) {
+                        var getbreadcrumbulname = getbreadcrumbul[counter].innerHTML;
+                        //alert(getbreadcrumbulname);
+                        if (DummyBreadCrumb != null && DummyBreadCrumb != "" && DummyBreadCrumb != "undefined") {
+                            DummyBreadCrumb += '>' + getbreadcrumbulname;
+                        } else {
+                            DummyBreadCrumb = getbreadcrumbulname;
+                        }
+                    }
+                    DummyBreadCrumb += '>' + 'Azure Arm';
+
+                    if (DummyBreadCrumb != null && DummyBreadCrumb != 'undefined') {
+                        localStorage.removeItem("breadcrumb");
+                        splitBread = DummyBreadCrumb.split('>');
+                        if (splitBread.length > 0) {
+                            $('#ribbon').find('.breadcrumb').find('li').detach();
+                            for (var arraycount = 0; arraycount < splitBread.length; arraycount++) {
+                                var liNew = document.createElement('li');
+                                liNew.innerHTML = splitBread[arraycount];
+                                $('#ribbon').find('.breadcrumb').append(liNew);
+                            }
+                        }
+                    }
+                    //$('#ribbon').find('.breadcrumb').append('<li>"'+ DummyBreadCrumb'"</li>');
+                    //alert(DummyBreadCrumb);
+                }
+
+            });
+
+
         }
 
         /*Initialising the data table in  orchestration*/
