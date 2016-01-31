1b6f66ec150d0e2d5005bfb9d3afd91fd2e1bc8d
Gobinda Das <gobinda.das@relevancelab.com>
2016-01-31 19:01:09 +0530

    Fix for nexus groupid

--- client/htmls/private/js/dev/awsProduction.js
+++ client/htmls/private/js/dev/awsProduction.js
@@ -1180,8 +1180,8 @@
 	                                alert("Please select repository.");
 	                                return false;
 	                            }*/
-	                            /*alert($(this).prop("checked"));
-	                            if ($(this).prop("checked")) {*/
+	                            /*alert($(this).prop("checked"));*/
+	                            if ($(this).prop("checked")) {
 	                                if (nexusServerType === "nexus") {
 	                                    var artifactId = $chooseArtifacts.find('option:selected').val();
 	                                    if (!artifactId) {
@@ -1226,7 +1226,7 @@
 	                                    };
 	                                    reqBody.docker = docker;
 	                                }
-	                            //}
+	                            }
 	                            console.log($('#compositedockertable').find('.dockerimagesrow').length);
 	                            $('.dockerimagesrow').each(function() {
 	                                dockerimages = {};
